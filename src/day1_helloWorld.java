8e6ce80cd9a36a13a970490e664273df84f1a847
blishchenko1981 <blishchenko1981@gmail.com>
2021-07-26 11:00:11 -0400

    initial commit

--- src/day1_helloWorld.java
+++ src/day1_helloWorld.java
@@ -1,7 +1,7 @@
 public class day1_helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Add line on GitHub");
+        System.out.println("Added  line on GitHub");
          
     }
 }
