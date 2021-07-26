91d5cc943bf6a721069e3916dc303972c3444e7d
blishchenko1981 <blishchenko1981@gmail.com>
2021-07-26 16:25:24 -0400

    set jdk

--- src/day1/day1_helloWorld.java
+++ src/day1/day1_helloWorld.java
@@ -5,6 +5,7 @@ public class day1_helloWorld {
         System.out.println("Hello world");
         System.out.println("Added  line on GitHub");
         System.out.println("More additions from GitHub");
+        System.out.println("dont forget set up jdk");
         
          
     }
