8ec6b2c8328b00e1f86426ae9aedf32449f631dc
blishchenko1981 <87575187+blishchenko1981@users.noreply.github.com>
2021-07-26 10:59:34 -0400

    Update day1_helloWorld.java

--- src/day1_helloWorld.java
+++ src/day1_helloWorld.java
@@ -1,5 +1,7 @@
 public class day1_helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Add line on GitHub");
+         
     }
 }
