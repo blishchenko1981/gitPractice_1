a7c2f8ad95eb8c17f1b02464fb31f0644fe9e035
blishchenko1981 <blishchenko1981@gmail.com>
2021-07-26 21:36:58 -0400

    case4

--- src/Vitalii/Day1.java
+++ src/Vitalii/Day1.java
@@ -3,4 +3,10 @@ package Vitalii;
 public class Day1 {
     //here I am begin to work
 
+// work 1 Is here I am Just begin.....
+    // case 2 done
+    //case3 is finished
+
+    // keep working on project case4
+
 }
